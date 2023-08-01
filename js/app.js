b2a9d36e616aa9a3d45a3bf83a680c98e1f45996
juanjopareja <jupg25@msn.com>
2023-08-01 19:37:04 +0200

    Add slider settings

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 window.swiper = new Swiper({
     el: '.slider__container',
     slideClass: 'slider__slide',
-    createElements: true
+    createElements: true,
+    autoplay: {
+        delay: 5000
+    },
+    loop: true
 });
